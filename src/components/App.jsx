d837812e42855d6aad7787b0f4d49233087173b0
vitalii-kyiv <v.s.chernous@gmail.com>
2024-02-05 11:33:52 +0200

    draft_1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import RenderProfile from './Profile/Profile';
+import userData from '../user.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,17 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      <RenderProfile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
     </div>
   );
 };
